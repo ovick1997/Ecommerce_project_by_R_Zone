91dfa7807105fe2179cd5c86cc36aadb804c045c
Md Shorov Abedin <mdovick1952@gmail.com>
2019-09-12 21:07:20 +0600

    database/migration/create_admins_table.php setup

--- database/migrations/2019_09_12_150230_create_admins_table.php
+++ database/migrations/2019_09_12_150230_create_admins_table.php
@@ -7,17 +7,22 @@ use Illuminate\Database\Migrations\Migration;
 class CreateAdminsTable extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('admins', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+    * Run the migrations.
+    *
+    * @return void
+    */
+   public function up()
+   {
+       Schema::create('admins', function (Blueprint $table) {
+       $table->bigIncrements('id');
+       $table->string('name');
+       $table->string('email')->unique();
+       $table->timestamp('email_verified_at')->nullable();
+       $table->string('password');
+       $table->rememberToken();
+       $table->timestamps();
+   });
+   }
 
     /**
      * Reverse the migrations.
