549b27755c36025ce72d07a99843070b32a97581
Md Shorov Abedin <mdovick1952@gmail.com>
2019-09-12 21:27:50 +0600

    Logout method create in logincontroller.php

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
- 
+
+use Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -52,5 +53,15 @@ class LoginController extends Controller
             return redirect()->intended(route('admin.dashboard'));
         }
         return back()->withInput($request->only('email', 'remember'));
+    }
+     /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+        $request->session()->invalidate();
+        return redirect()->route('admin.login');
     }
 }
